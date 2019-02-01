a602031dc0a17cf334b7c5496249f404f94b54fa
James Singletary <jsingletaryiv@gmail.com>
2019-02-01 15:02:22 -0500

    Still dealing with this TS Erorr - Deleting entire project so I can recreate the application via Angular CLI.

--- src/app/core/index.ts
+++
@@ -1,2 +0,0 @@
-export * from '@app/core/core.module';
-export * from '@app/core/navbar/navbar.component';
--- src/app/shared/index.ts
+++
@@ -1,4 +0,0 @@
-export * from '@app/shared/shared.module';
-export * from '@app/shared/icons/icons.module';
-export * from '@app/shared/footer/footer.component';
-// export * from '@app/shared/material/material.module';
